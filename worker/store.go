a78d284f7797262868bebfd2ebb9a8f8e50e017d
Greg Poirier <greg@opsee.com>
2016-06-09 10:35:25 -0700

    whoops. also make it check_states

--- worker/store.go
+++ worker/store.go
@@ -13,8 +13,8 @@ import (
 // assumes a present state of OK.
 func GetState(q sqlx.Ext, customerId, checkId string) (*State, error) {
 	state := &State{}
-	err := sqlx.Get(q, state, "SELECT cs.id, cs.customer_id, cs.check_id, cs.state, cs.time_entered, cs.last_update, c.min_failing_count, c.min_failing_time, cs.num_failing FROM check_state AS cs JOIN checks AS c ON (c.id=cs.check_id) WHERE cs.customer_id=? AND cs.id=?", customerId, checkId)
-	if err != nil && != sql.ErrNoRows {
+	err := sqlx.Get(q, state, "SELECT cs.id, cs.customer_id, cs.check_id, cs.state, cs.time_entered, cs.last_update, c.min_failing_count, c.min_failing_time, cs.num_failing FROM check_states AS cs JOIN checks AS c ON (c.id=cs.check_id) WHERE cs.customer_id=? AND cs.id=?", customerId, checkId)
+	if err != nil && err != sql.ErrNoRows {
 		return nil, err
 	}
 
@@ -56,6 +56,6 @@ func GetState(q sqlx.Ext, customerId, checkId string) (*State, error) {
 }
 
 func PutState(q sqlx.Ext, state *State) error {
-	_, err := sqlx.NamedExec(q, "INSERT INTO check_state (check_id, customer_id, state_id, state_name, time_entered, last_update) VALUES (:check_id, :customer_id, :state_id, :state_name, :time_entered, :last_update, :num_failing) ON CONFLICT UPDATE", state)
+	_, err := sqlx.NamedExec(q, "INSERT INTO check_states (check_id, customer_id, state_id, state_name, time_entered, last_update) VALUES (:check_id, :customer_id, :state_id, :state_name, :time_entered, :last_update, :num_failing) ON CONFLICT UPDATE", state)
 	return err
 }
