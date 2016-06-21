aabba702d2bb1e15bf39b7733a52c9071245ebca
Greg Poirier <greg@opsee.com>
2016-06-21 13:38:32 -0700

    Ignore bad checkresults

--- cmd/worker/main.go
+++ cmd/worker/main.go
@@ -75,6 +75,18 @@ func main() {
 			return err
 		}
 
+		logger := log.WithFields(log.Fields{
+			"customer_id": result.CustomerId,
+			"check_id":    result.CheckId,
+			"bastion_id":  result.BastionId,
+		})
+
+		// TODO(greg): CheckResult objects should probably have a validator.
+		if result.CustomerId == "" || result.CheckId == "" {
+			log.Error("Received invalid check result.")
+			return nil
+		}
+
 		// TODO(greg): Once all bastions have been upgraded to include Bastion ID in
 		// their check results, everything in this block can be deleted.
 		// -----------------------------------------------------------------------
@@ -108,11 +120,6 @@ func main() {
 		// -----------------------------------------------------------------------
 
 		// For now, the region is just static, because we only have dynamodb in one region.
-		logger := log.WithFields(log.Fields{
-			"customer_id": result.CustomerId,
-			"check_id":    result.CheckId,
-			"bastion_id":  result.BastionId,
-		})
 
 		dynamo := &worker.DynamoStore{dynamodb.New(session.New(&aws.Config{Region: aws.String("us-west-2")}))}
 		task := worker.NewCheckWorker(db, dynamo, result)
